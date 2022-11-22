7bf3956ca5eb5f94c6738ed24ad0e8895160bdde
Hyeran Lee <hyeranlee@MacBook-Pro-2.local>
2022-11-22 07:51:13 -0500

    add .clone on GET

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
 import express from "express";
 import mongoose from "mongoose";
-import noteModel from "./models/notes.js"
-import cors from "cors"
-import dotenv from "dotenv"
+import noteModel from "./models/notes.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(process.env.ATLAS_URI);
 
-
-mongoose.connect(process.env.ATLAS_URI)
-
-
-
-app.get ("/getNotes", (req,res) =>{
-    noteModel.find({},(err, result) =>{
-        if (err) {
-            res.json(err)
-        } else {
-            res.json(result)
-        }
+app.get("/getNotes", async (req, res) => {
+  await noteModel
+    .find({}, (err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
     })
+    .clone()
+    .catch(function (err) {
+      console.log(err);
+    });
 });
 
-app.post ("/createNote", async (req,res) => {
-    const note = req.body;
-    const newNote = new noteModel(note);
-    await newNote.save();
+app.post("/createNote", async (req, res) => {
+  const note = req.body;
+  const newNote = new noteModel(note);
+  await newNote.save();
 
-    res.json(note);
-})
-
-app.put("/updateNote", async (req,res) =>{
-    const updatedContent= req.body.updatedContent
-    const id = req.body.id;
-
-    try {
-        await noteModel.findById(id, (err, contentToUpdate)=>{
-            contentToUpdate.content = updatedContent;
-            contentToUpdate.save();
-        }).clone();
-    } catch(err){
-        console.log(err)
-    }
-    res.send("updated")
+  res.json(note);
 });
 
-app.delete("/deleteNote/:id", async (req, res)=>{
-    const id = req.params.id
-    await noteModel.findByIdAndRemove(id).exec()
-    res.send("note deleted") 
-})
+app.put("/updateNote", async (req, res) => {
+  const updatedContent = req.body.updatedContent;
+  const id = req.body.id;
 
-app.listen(3001,()=>{
-    console.log(`listening on : 3001`)
+  try {
+    await noteModel
+      .findById(id, (err, contentToUpdate) => {
+        contentToUpdate.content = updatedContent;
+        contentToUpdate.save();
+      })
+      .clone();
+  } catch (err) {
+    console.log(err);
+  }
+  res.send("updated");
+});
+
+app.delete("/deleteNote/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findByIdAndRemove(id).exec();
+  res.send("note deleted");
+});
+
+app.listen(3001, () => {
+  console.log(`listening on : 3001`);
 });
